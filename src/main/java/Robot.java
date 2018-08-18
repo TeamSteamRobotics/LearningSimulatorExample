957f9198121084f861c68f07920706c7db679130
amrelk <malcolmmaude01@gmail.com>
2018-08-18 17:49:14 -0500

    inital pt2

---
+++ src/main/java/Robot.java
@@ -0,0 +1,15 @@
+import amrelk.simulation.learning.RobotBase;
+
+
+public class Robot extends RobotBase {
+
+    @Override
+    public void robotInit() {
+
+    }
+
+    @Override
+    public void robotPeriodic() {
+        System.out.println("hi");
+    }
+}
