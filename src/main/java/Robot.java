2ed474eef60d650663c2c8d5f767697d60dbbea1
amrelk <malcolmmaude01@gmail.com>
2018-08-29 10:40:17 -0500

    added separate modes

--- src/main/java/Robot.java
+++ src/main/java/Robot.java
@@ -1,16 +1,35 @@
-import amrelk.simulation.learning.RobotBase;
+import amrelk.simulation.learning.*;
 
 
 public class Robot extends RobotBase {
 
+    Joystick stick;
+    Talon leftMotor;
+    Talon rightMotor;
+    Drive drive;
+    Encoder leftEncoder;
+    Encoder rightEncoder;
+
     @Override
     public void robotInit() {
+        stick  = new Joystick(0);
 
+        leftMotor = new Talon(0);
+        rightMotor = new Talon(1);
+        drive = new Drive(leftMotor, rightMotor);
+
+        leftEncoder = new Encoder(2, 3);
+        rightEncoder = new Encoder(4, 5);
     }
 
     @Override
-    public void robotPeriodic() {
-        setLeftWheel(counter % 1000 > 500 ? 1 : .75);
-        setRightWheel(counter % 1000 > 500 ? .75 : 1);
+    public void autonomousPeriodic() {
+        drive.drive(1, .1);
+        System.out.println((double)leftEncoder.get()/2048.0);
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        drive.drive(stick.getY(), stick.getX());
     }
 }
